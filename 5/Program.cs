223c003c03958bf0848b0ce3b64d91d234bc65e6
Anthony Roberts <anthony.roberts@linaro.org>
2024-12-05 17:18:57 +0000

    Day 5: Switch to pre-computed grouping instead of GroupBy

--- 5/Program.cs
+++ 5/Program.cs
@@ -7,8 +7,29 @@ var inputFromFile = File.ReadAllLines(args.Length > 0 ? args[0] : "..\\..\\..\\i
 // Parse out the rules into int[2] arrays
 var orderingRules = inputFromFile.TakeWhile(x => !string.IsNullOrWhiteSpace(x)).Select(y => Array.ConvertAll(y.Split('|'), int.Parse));
 
+Dictionary<int, List<int>> beforeOrderingRules = new Dictionary<int, List<int>>(100);
+Dictionary<int, List<int>> afterOrderingRules = new Dictionary<int, List<int>>(100);
+
+foreach(var orderingRule in orderingRules)
+{
+    if(!beforeOrderingRules.ContainsKey(orderingRule[0]))
+    {
+        // In testing there were never more than 30 rules of a certain grouping
+        beforeOrderingRules[orderingRule[0]] = new List<int>(30);
+    }
+
+    if(!afterOrderingRules.ContainsKey(orderingRule[1]))
+    {
+        // In testing there were never more than 30 rules of a certain grouping
+        afterOrderingRules[orderingRule[1]] = new List<int>(30);
+    }
+
+    beforeOrderingRules[orderingRule[0]].Add(orderingRule[1]);
+    afterOrderingRules[orderingRule[1]].Add(orderingRule[0]);
+}
+
 // Group them by the first half of the rule (Part 1)
-var orderingRulesBefore = orderingRules.GroupBy(z => z[0]).ToList();
+//var orderingRulesBefore = orderingRules.GroupBy(z => z[0]).ToList();
 
 // Parse out the page numbers
 var pageNumbers = inputFromFile.Skip(orderingRules.Count() + 1).Select(y => Array.ConvertAll(y.Split(','), int.Parse)).ToArray();
@@ -16,21 +37,21 @@ var pageNumbers = inputFromFile.Skip(orderingRules.Count() + 1).Select(y => Arra
 // Checks if pages are in order
 // Note: We don't care about binary size or compilation time for AoCbench, so force inlining
 [MethodImpl(MethodImplOptions.AggressiveInlining)]
-bool ArePagesInOrder(int[] pages)
+bool ArePagesInOrder(Span<int> pages)
 {
     // Loop through every page and check if it violates any rule numbers
     for (int i = 0; i < pages.Length; i++)
     {
         // Find the relevant rules (first-element grouped) for this page number
-        var rulesForThisNumber = orderingRulesBefore.Where(x => x.Key == pages[i]);
-        if (rulesForThisNumber.Any())
+        //var rulesForThisNumber = beforeOrderingRules.Where(x => x.Key == pages[i]);
+        if (beforeOrderingRules.ContainsKey(pages[i]))
         {
             // Look back at previous pages to see if they violate the rules
             Span<int> slicedPages = pages[..i];
-            foreach (var rule in rulesForThisNumber)
+            foreach (var rule in beforeOrderingRules[pages[i]])
             {
                 // See if there is a rule that matches - if there is, this is an invalid sequence of pages
-                var pageRuleIndex = slicedPages.IndexOfAny(rule.Select(x => x[1]).ToArray());
+                var pageRuleIndex = slicedPages.IndexOf(rule);
                 if(pageRuleIndex != -1) return false;
             }
         }
@@ -58,13 +79,13 @@ void Part1And2()
             // This is a really interesting property of the data/rules I noticed while debugging that results in a ridiculous (and likely unintended) perf win:
             // You don't actually need to order it - for all possible valid rules for this number set, when grouped by the second number in the rule - 
             // the group with the same number of rules as the index of the middle value of the array, matches the correct middle number
-            var validSecondNoGroups = orderingRules.Where(x => pageNumberSet.Contains(x[0]) && pageNumberSet.Contains(x[1])).GroupBy(z => z[1]);
+            var validSecondNoGroups = afterOrderingRules.Select(x => (x.Key, x.Value.Where(y => pageNumberSet.Contains(y)).ToArray())).Where(x => pageNumberSet.Contains(x.Item1) && x.Item2.Length > 0).ToArray();
             //p2Counter += .First(w => w.Count() == pageNumberSet.Length / 2).Key;
             foreach(var validGroup in validSecondNoGroups)
             {
-                if(validGroup.Count() == pageNumberSet.Length / 2)
+                if(validGroup.Item2.Count() == pageNumberSet.Length / 2)
                 {
-                    p2Counter += validGroup.Key;
+                    p2Counter += validGroup.Item1;
                     break;
                 }
             }
